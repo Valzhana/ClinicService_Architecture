524407d1ec34c9b88f1c62dd5f9aee1d2224e535
Olga Gladkaya <tarantasik@mail.ru>
2023-11-22 00:53:25 +0300

    Добавьте файлы проекта.

---
+++ ClinicService/Models/Requests/UpdatePetRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace ClinicService.Models.Requests
+{
+    public class UpdatePetRequest
+    {
+        /// <summary>
+        /// ID животного в БД
+        /// </summary>
+        public int PetId { get; set; }
+
+        /// <summary>
+        /// Клиент к которому принадлежит животное
+        /// </summary>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Кличка животного
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Дата рождения животного
+        /// </summary>
+        public DateTime Birthday { get; set; }
+    }
+}
